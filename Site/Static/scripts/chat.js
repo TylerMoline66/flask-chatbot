9362538536ad984db66c9a44f17ac7c0d0905ffd
Tyler Moline <tylermoline95@gmail.com>
2024-04-04 22:18:11 -0600

    refined front end

--- Site/Static/scripts/chat.js
+++ Site/Static/scripts/chat.js
@@ -2,7 +2,16 @@ function sendMessage() {
   let message = document.getElementById("user_input").value;
 
   if (message.length > 0) {
+    const userChat = document.createElement("div");
+    const chatContainer = document.querySelector(".chat-container");
+
+    userChat.classList.add("user-chat");
+    userChat.innerHTML = `<p>${message}</p>`;
+
+    chatContainer.appendChild(userChat);
+
     document.getElementById("user_input").value = "";
+
     fetch("/chat", {
       method: "POST",
       headers: {
@@ -12,13 +21,13 @@ function sendMessage() {
     })
       .then((response) => response.json())
       .then((data) => {
-        const chatDiv = document.getElementById("chat");
-        const userDiv = document.createElement("div");
-        userDiv.innerText = "You: " + message;
-        chatDiv.appendChild(userDiv);
-        const botDiv = document.createElement("div");
-        botDiv.innerText = "Bot: " + data.message;
-        chatDiv.appendChild(botDiv);
+        const chatContainer = document.querySelector(".chat-container");
+        const aiChat = document.createElement("div");
+
+        aiChat.classList.add("ai-chat");
+        aiChat.innerHTML = `<p>${data.message}</p>`;
+
+        chatContainer.appendChild(aiChat);
       });
   } else {
     const chatDiv = document.getElementById("chat");
--- app.py
+++ app.py
@@ -7,15 +7,15 @@ from db import *
 app = Flask(__name__, template_folder="site/templates", static_folder="Site/Static")
 
 
-# SQLAlchemy configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://test@test.com:1234!@localhost/chatbot'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# # SQLAlchemy configurations
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://test@test.com:1234!@localhost/chatbot'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+# db.init_app(app)
 
-# Create all database tables
-with app.app_context():
-    db.create_all()
+# # Create all database tables
+# with app.app_context():
+#     db.create_all()
 
 
 app.register_blueprint(Routes.static_blueprint)
